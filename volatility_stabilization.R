9f19f1e202704c3083d7e6422bc6366c67d78135
Rangel <rangel@smn.at>
2025-01-08 17:11:35 +0100

    Finished vol stabilization function. Need to write tail exponent estimation within the function.

--- volatility_stabilization.R
+++ volatility_stabilization.R
@@ -2,6 +2,7 @@
 #Download data from yahoo
 spx <- yahoo_load(tickers = "^GSPC", from = "1995-01-01")
 spx[, return:=AdjustedClose/shift(AdjustedClose)-1]
+spx[is.na(return), return:=0]
 spx[, return_log:=c(0, exp(diff(log(AdjustedClose)))-1)]
 
 #20 day rolling volatility
@@ -31,10 +32,40 @@ end-start
 plot(spx$Date, spx$vol_ewma, type="l")
 lines(spx$Date, spx$vol_stable, type="l", col="red4")
 
-vol_stabilization <- function(returns, convert_to_log=F, vol_window=20, tail_exp_window,
-                              tail_exp_method=c("MLE", "MOM", "PWM")){
+#GPD estimation using absolute returns
+spx[,abs_return:=abs(return)]
+quantile(spx$abs_return, 0.9)
+abs_rets <- spx$abs_return
+excesses <- abs_rets[abs_rets>quantile(abs_rets, 0.9)]-quantile(abs_rets, 0.9)
+fit_GPD_PWM(x = excesses)
+
+vol_stabilization <- function(returns, convert_to_log=F, vol_window=20, lambda=0.03, 
+                              tail_exp_window=3000, 
+                              tail_exp_window_type=c("moving", "expanding")){
   if(convert_to_log){
     returns <- log(returns+1)
   }
-  
+  dtab <- as.data.table(returns)
+  #N-time points rolling std. dev.
+  dtab[, vol:=frollapply(returns, n = vol_window, FUN = function(x) sd(x))]
+  #EWMA 2 day volatility with lambda of 0.03
+  dtab[, vol_ewma:=sqrt((1-lambda)*shift(vol)^2+lambda*returns^2)]
+  #Assume tail exponent of 2.4 - will later be expanded to MLE estimation of Pareto dist.
+  tail_exp <- 2.4
+  dtab[, vol_stable:=numeric(nrow(dtab))]
+  dtab[vol_window+1, vol_stable:=vol]
+  for(i in (vol_window+2):nrow(dtab)){
+    vol_before <- dtab[i-1, vol_stable]
+    dtab[i, vol_stable:=fifelse(abs(returns)<=tail_exp*vol_before,
+                               sqrt((1-lambda)*vol_before^2+lambda*returns^2),
+                               vol_before)]
+  }
+  dtab
 }
+
+vol_1<-vol_stabilization(returns = spx$return, tail_exp_window_type = "expanding")
+plot(spx$Date, spx$vol, type="l")
+lines(spx$Date, spx$vol_stable, type="l", col="red4")
+
+plot(vol_1$vol, type="l")
+lines(vol_1$vol_stable, col="red4")
